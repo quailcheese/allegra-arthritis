5d14b55306b59736d508c58b3f9249fc53bdcd28
Jason Lee <jasonlee@Jasons-MacBook-Pro.local>
2025-07-09 15:12:58 -0400

    update hours

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -50,7 +50,8 @@ const LocationMap: React.FC = () => {
       lng: -74.1487,
       hours: [
         "Tuesday: 8:30am - 3:00pm",
-        "Friday: 8:30am - 3:00pm"
+        "Thursday: 8:30am - 4:00pm",
+        "Friday: 8:30am - 1:00pm"
       ]
     },
     {
@@ -60,8 +61,8 @@ const LocationMap: React.FC = () => {
       lat: 40.1451,
       lng: -74.2436,
       hours: [
-        "Wednesday: 9:00am - 2:45pm",
-        "Friday: 9:00am - 12:45pm"
+        "Wednesday: 8:30am - 4:00pm",
+        "Friday: 9:00am - 1:00pm"
       ]
     }
   ];
